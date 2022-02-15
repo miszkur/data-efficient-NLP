188f0d0fcbbf4990314c2d07d870e1c0b1ca4589
miszkur <miszkurka.aga@gmail.com>
2022-02-15 17:48:35 +0100

    change dropout to 0.1

--- src/config/config.py
+++ src/config/config.py
@@ -5,7 +5,7 @@ def bert_tiny():
   config = ml_collections.ConfigDict()
   config.bert_version = 'prajjwal1/bert-tiny' #'bert-base-cased' 
   config.output_dim = 128
-  config.dropout = 0.2
+  config.dropout = 0.1
   config.num_classes = 8
   return config
 
@@ -13,7 +13,7 @@ def bert_config():
   config = ml_collections.ConfigDict()
   config.bert_version = 'bert-base-cased' 
   config.output_dim = 768
-  config.dropout = 0.2
+  config.dropout = 0.1
   config.num_classes = 8
   return config
 
