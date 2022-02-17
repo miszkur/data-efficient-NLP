2a770f22509e321a2d3662a6149442f48138c74a
miszkur <miszkurka.aga@gmail.com>
2022-02-17 17:45:34 +0100

    add f1 score

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,5 +1,5 @@
 from data_processing.ev_parser import create_dataloader
-from torchmetrics import HammingDistance
+from torchmetrics import HammingDistance, F1Score
 from sklearn.metrics import classification_report
 from tqdm import tqdm
 
@@ -8,18 +8,21 @@ import numpy as np
 import torch
 
 
-hamming_distance = HammingDistance()
 
-def compute_accuracy(output, labels):
+hamming_distance = HammingDistance()
+f1 = F1Score(num_classes=8)
+
+
+def compute_accuracy_and_f1(output, labels):
   """Compute accuracy accprding to the following formula:
   accuracy = 1 - hamming distance"""
   with torch.no_grad():
     preds = torch.sigmoid(output).cpu()
     target = labels.cpu().to(torch.int)
     acc = hamming_distance(preds, target)
-    return 1 - acc.item()
+    return 1 - acc.item(), f1(preds, target)
 
-def evaluate(model, config, test_save_path=None):
+def print_evaluation_report(model, config, test_save_path=None):
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
   test_loader, target_names = create_dataloader(config, 'test', return_target_names=True)
 
@@ -27,6 +30,7 @@ def evaluate(model, config, test_save_path=None):
   criterion = nn.BCEWithLogitsLoss()
   loss = 0
   accuracy = 0
+  f1_score = 0
   y_true = []
   y_pred = []
   with torch.no_grad():
@@ -39,20 +43,21 @@ def evaluate(model, config, test_save_path=None):
       output = model(input_ids=inputs, attn_mask=attention_masks, token_type_ids=token_type_ids)
       loss = criterion(output, labels)
       loss += loss.item()
-      accuracy += compute_accuracy(output, labels)
+      acc, f1_s = compute_accuracy_and_f1(output, labels)
+      accuracy += acc
+      f1_score += f1_s
       
       y_true.append(labels.cpu().numpy())
       y_pred.append(np.round(torch.sigmoid(output).cpu().numpy()))
 
   loss /= len(test_loader)
   accuracy /= len(test_loader)
-  print(f'Test loss: {loss:.4f}, Test accuracy: {accuracy:.4f}')
+  f1_score /= len(test_loader)
+  print(f'Test loss: {loss:.4f}, accuracy: {accuracy:.4f}, F1-score: {f1_score:.4f}')
 
   print('Per-class metrics')
   y_true = np.concatenate(y_true)
-  y_pred = np.concatenate(y_pred)
-  print(y_pred)
-  print(y_true)
+  y_pred = np.concatenate(y_pred)  
   print(classification_report(y_true, y_pred, target_names=target_names))
 
   print('Per-class accuracy')
