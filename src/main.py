ab78483a502c4240c23d01d9f4e179f2e9711beb
miszkur <miszkurka.aga@gmail.com>
2022-02-21 16:02:25 +0100

    add entropy to AL strategies

--- src/main.py
+++ src/main.py
@@ -3,31 +3,41 @@ import config.config as configs
 import torch
 import os
 
-from run_experiment import run_active_learning_experiment
+from run_experiment import run_active_learning_experiment, Strategy
 from models.bert import BertClassifier
 from train import Learner
 from evaluate import print_evaluation_report
+from visualisation.active_learning import plot_al_results
 
 
 def main():
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  # config = configs.active_learning_config()
-  # results = run_active_learning_experiment(config, device)
 
-  config = configs.multilabel_base()
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('--experiment', type=str,
+                      help='Experiment name')
+  parser.add_argument('--al_strategy', type=str,
+                      help='AL strategy to use e.g. RANDOM, ENTROPY')
+  args = parser.parse_args()
 
-  model = BertClassifier(config=config.bert) 
-  model.to(device)
-  # Train
-  learner = Learner(device, model, config.results_dir)
-  learner.train(config)
+  if args.experiment == 'al':
+    config = configs.active_learning_config()
+    assert args.al_strategy is not None
+    config.query_strategy = args.al_strategy
+    results = run_active_learning_experiment(config, device, Strategy[args.al_strategy])
+    plot_al_results(config)
+  elif args.experiment == 'supervised':
+    config = configs.multilabel_base()
+    model = BertClassifier(config=config.bert) 
+    model.to(device)
+    # Train
+    learner = Learner(device, model, config.results_dir)
+    learner.train(config)
 
-  # Test
-  save_model_path = os.path.join(config.results_dir, 'bert' + '.pth')
-  model.load_state_dict(torch.load(save_model_path), strict=False)
-  print_evaluation_report(model, config)
+    # Test
+    save_model_path = os.path.join(config.results_dir, 'bert' + '.pth')
+    model.load_state_dict(torch.load(save_model_path), strict=False)
+    print_evaluation_report(model, config)
 
 
 main()
-
-
--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,10 +1,13 @@
 import ml_collections
+import numpy as np
 import torch
 import os
 
 from torch.utils.data import DataLoader, random_split, ConcatDataset
 from abc import abstractmethod
 from typing import Dict, List
+from enum import Enum
+from tqdm import tqdm
 
 from data_processing.ev_parser import create_dataset, create_dataloader
 from visualisation.active_learning import plot_al_results
@@ -12,24 +15,28 @@ from config.config import multilabel_base
 from models.bert import BertClassifier
 from train import Learner
 
+class Strategy(Enum):
+  RANDOM = 1
+  ENTROPY = 2
 
 class QueryStrategy:
   """Base class for Active Learning query strategies."""
-  def __init__(self, dataset, sample_size, seed):
+  def __init__(self, dataset, sample_size, batch_size):
     self.sample_size = sample_size
-    self.generator = torch.Generator().manual_seed(seed)
     self.dataset = dataset
+    self.batch_size = batch_size
 
   @abstractmethod
-  def choose_samples_to_label(self):
+  def choose_samples_to_label(self, learner: Learner):
     pass
 
 class RandomStrategy(QueryStrategy):
   """Chooses samples to label uniformly at random."""
-  def __init__(self, dataset, sample_size=48, seed=42):
-    super().__init__(dataset, sample_size, seed)
+  def __init__(self, dataset, sample_size=48, batch_size=8, seed=42):
+    super().__init__(dataset, sample_size, batch_size)
+    self.generator = torch.Generator().manual_seed(seed)
 
-  def choose_samples_to_label(self):
+  def choose_samples_to_label(self, learner: Learner):
     data_to_label, unlabeled_data = random_split(
       self.dataset, 
       [self.sample_size, len(self.dataset)-self.sample_size], 
@@ -37,9 +44,51 @@ class RandomStrategy(QueryStrategy):
     self.dataset = unlabeled_data
     return data_to_label
 
+class MaxEntropyStrategy(QueryStrategy):
+  """Chooses samples to label which have the maximum value of normalized entropy."""
+  def __init__(self, dataset, sample_size=48, batch_size=8):
+    super().__init__(dataset, sample_size, batch_size)
+
+  def choose_samples_to_label(self, learner: Learner):
+    data_loader = DataLoader(
+      self.dataset, 
+      batch_size=self.batch_size,
+      shuffle=False)
+
+    entropies = []
+    learner.model.eval()
+    with torch.no_grad():
+      for batch in tqdm(data_loader):
+        output = learner.inference(batch) # batch_size x num_classes
+        p = torch.sigmoid(output).cpu().detach().numpy()
+        entropy = -(p*np.log(p)+(1-p)*np.log(1-p))
+        normalized_entropy = np.sum(entropy, axis=1) / entropy.shape[1]
+        entropies.append(normalized_entropy)
+
+    # Get the data to label based on entropy values.  
+    entropies = np.concatenate(entropies)
+    print(entropies)
+    sorted_indices = np.flip(np.argsort(entropies))
+    indices_to_label = sorted_indices[:self.sample_size]
+    print(len(indices_to_label))
+    data_to_label = torch.utils.data.Subset(self.dataset, indices_to_label)
+
+    # Get the rest of dataset (unlabeled).
+    all_indices = np.arange(len(self.dataset))
+    unlabeled_data_indices = all_indices[~np.in1d(all_indices, indices_to_label)]
+    self.dataset = torch.utils.data.Subset(self.dataset, unlabeled_data_indices)
+    return data_to_label
+
+def initialize_strategy(strategy: Strategy, train_dataset, config, seed):
+  if strategy == Strategy.RANDOM:
+    return RandomStrategy(train_dataset, config.sample_size, config.batch_size, seed)
+  elif strategy == Strategy.ENTROPY:
+    return MaxEntropyStrategy(train_dataset, config.sample_size, config.batch_size)
 
 def run_active_learning_experiment(
-  config: ml_collections.ConfigDict, device: str) -> Dict[str, List[float]]:
+  config: ml_collections.ConfigDict, 
+  device: str, 
+  strategy: Strategy) -> Dict[str, List[float]]:
   """Run Active Learning experiment. 
   
   Save results to the folder specified in the config.
@@ -62,8 +111,13 @@ def run_active_learning_experiment(
 
   for i, seed in enumerate(config.seeds):
     print(f'=== Active Learning experiment for seed {i+1}/{len(config.seeds)} ===')
-    rqs = RandomStrategy(train_dataset, config.sample_size, seed)
-    labeled_data = rqs.choose_samples_to_label()
+    strategy = initialize_strategy(strategy, train_dataset, config, seed)
+    
+    model = BertClassifier(config=config.bert) 
+    model.to(device)
+    learner = Learner(device, model)
+
+    labeled_data = strategy.choose_samples_to_label(learner)
     for i in range(num_al_iters):
       results['split'].append(len(labeled_data))
       train_loader = DataLoader(
@@ -78,10 +132,11 @@ def run_active_learning_experiment(
       learner.train(config, train_loader)
       # Test
       loss, accuracy, f1_score = learner.evaluate(test_loader)
+      print(f'Test loss: {loss}, accuracy: {accuracy}, f1 score: {f1_score}')
       results['accuracy'].append(accuracy)
       results['f1_score'].append(f1_score)
-
-      new_labeled_data = rqs.choose_samples_to_label()
+      
+      new_labeled_data = strategy.choose_samples_to_label(learner)
       labeled_data = ConcatDataset([labeled_data, new_labeled_data])
 
   print('Saving results..')
