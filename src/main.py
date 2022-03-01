10023f75d606dba219e706cff4be1b43df85535a
miszkur <miszkurka.aga@gmail.com>
2022-03-01 14:50:07 +0100

    code for supervised baseline

--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ import config.config as configs
 import torch
 import os
 import active_learning.visualisation as al_vis
+import pickle
+import data_processing.utils as data_util
 
 from run_experiment import run_active_learning_experiment, Strategy
 from models.bert import BertClassifier
 from learner import Learner
 from evaluate import print_evaluation_report
 from active_learning.visualisation import plot_al_results
+from data_processing.ev_parser import create_dataloader
 
 
 def main():
@@ -19,6 +22,9 @@ def main():
                       help='Experiment name')
   parser.add_argument('--al_strategy', type=str,
                       help='AL strategy to use e.g. RANDOM, MAX_ENTROPY, AVG_ENTROPY')
+  parser.add_argument('--al_class', type=int, default=None,
+                      help='Class which will guide AL.')
+
   args = parser.parse_args()
 
   if args.experiment == 'al':
@@ -26,22 +32,37 @@ def main():
     allowed_query_strategies = [s.name for s in Strategy]
     
     assert args.al_strategy in allowed_query_strategies, f'Please specify a valid AL strategy: {allowed_query_strategies}'
+   
     config.query_strategy = args.al_strategy
-    # results = run_active_learning_experiment(config, device, Strategy[args.al_strategy])
+    results = run_active_learning_experiment(config, device, Strategy[args.al_strategy], al_class=args.al_class)
+    # Visualisation
     # al_vis.cold_vs_warm_start('AVG_ENTROPY', config)
     plot_al_results(['RANDOM', 'AVG_ENTROPY', 'MAX_ENTROPY', 'CAL'], config)
   elif args.experiment == 'supervised':
-    config = configs.multilabel_base()
-    model = BertClassifier(config=config.bert) 
-    model.to(device)
-    # Train
-    learner = Learner(device, model, config.results_dir)
-    learner.train(config)
+    valid_loader, test_loader = create_dataloader(config, 'valid')
+    results = {'accuracy': [], 'f1_score':[]}
+    for _ in range(5):
+      config = configs.multilabel_base()
+      model = BertClassifier(config=config.bert) 
+      model.to(device)
+      # Train
+      learner = Learner(device, model, config.results_dir)
+      learner.train(config, validation_loader=valid_loader)
+      
+      # Evaluate
+      loss, accuracy, f1_score = learner.evaluate(test_loader)
+      print(f'Test loss: {loss}, accuracy: {accuracy}, f1 score: {f1_score}')
+      results['accuracy'].append(accuracy)
+      results['f1_score'].append(f1_score)
 
+      print('Saving results..')
+      results_path = os.path.join(config.results_dir, f'SUPERVISED.pkl')
+      with open(results_path, 'wb') as fp:
+        pickle.dump(results, fp)
     # Test
-    save_model_path = os.path.join(config.results_dir, 'bert' + '.pth')
-    model.load_state_dict(torch.load(save_model_path), strict=False)
-    print_evaluation_report(model, config)
+    # save_model_path = os.path.join(config.results_dir, 'bert' + '.pth')
+    # model.load_state_dict(torch.load(save_model_path), strict=False)
+    # print_evaluation_report(model, config)
 
 
 main()
