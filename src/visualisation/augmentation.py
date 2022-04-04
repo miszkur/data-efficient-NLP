7487e088cef96ac7a7e88d77a37aad0e63f63172
miszkur <miszkurka.aga@gmail.com>
2022-04-04 20:00:28 +0200

    visualise small aug experiment

--- src/visualisation/augmentation.py
+++ src/visualisation/augmentation.py
@@ -2,13 +2,12 @@ import os
 import pickle
 import numpy as np
 
-class_names = ['functionality', 'range_anxiety']
+CLASS_NAMES = ['functionality','range_anxiety','availability','cost','ui','location','service_time','dealership']
 
 def print_summary(results):
-  keys = ['accuracy', 'f1_score', 'train_time', 0, 1, 'training_emissions']
   for key in results.keys():
-    if key in [0,1]:
-      print(f'Results for class {class_names[key]}')
+    if type(key) is int:
+      print(f'Results for class {CLASS_NAMES[key]}')
       print_summary(results[key])
     else:
       print(f'{key}: {np.mean(results[key]):.2f} ({np.std(results[key]):.2f})')
@@ -16,6 +15,20 @@ def print_summary(results):
 def visualise_full_data_results(config):
   no_aug_results_path = os.path.join(config.results_dir, '..', 'supervised', f'SUPERVISED.pkl')
   results_path = os.path.join(config.results_dir, f'SUPERVISED.pkl')
+
+  print('With data augmentation: ')
+  with open(results_path, 'rb') as f:
+    results = pickle.load(f)
+    print_summary(results)
+  
+  print('\nWithout data augmentation: ')
+  with open(no_aug_results_path, 'rb') as f:
+    results = pickle.load(f)
+    print_summary(results)
+
+def visualise_small_data_results(config):
+  no_aug_results_path = os.path.join(config.results_dir, 'small', f'SUPERVISED_300_aug_False.pkl')
+  results_path = os.path.join(config.results_dir, 'small', f'SUPERVISED_300_aug_True.pkl')
 
   print('With data augmentation: ')
   with open(results_path, 'rb') as f:
