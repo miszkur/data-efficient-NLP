bc3cf4a9ca080b12b4ba9fd2444e2de4d25d5143
miszkur <miszkurka.aga@gmail.com>
2022-05-06 11:25:50 +0200

    make figures from zero shot readable

--- src/visualisation/zero_shot.py
+++ src/visualisation/zero_shot.py
@@ -22,16 +22,15 @@ def visualise_per_class_performance(
       print(labels)
     else:
       labels = config.class_names
-    fig, axes = plt.subplots(len(metrics),1, figsize=(6, 9))
+    fig, axes = plt.subplots(len(metrics),1, figsize=(5, 9))
     for metric, ax in zip(metrics, axes.flatten()):
       metric_results = []
       for label in labels:
         # label = label.replace(' ', '_') # range anxiety => range_anxiety.
         metric_results.append(results[label][metric])
 
-      # plt.figure()
-      ax.set_title(f'{metric}')
-      # plt.xlabel(metric)
+      ax.set_title(f'{metric}', {'fontweight': "bold"})
+      ax.title.set_size(14)
       ax = sns.barplot(x=labels, y=metric_results, ax=ax)
       ax.set_xticklabels([])
       ax.set_ylim([0,1])
@@ -51,7 +50,9 @@ def visualise_per_class_performance(
     print('\hline\hline')
     print(f'sum & {clicks_preannotation} & {clicks_no_preannotation}')
 
+    plt.xticks(fontsize=14)
     ax.set_xticklabels(labels)
-    ax.tick_params(axis='x', rotation=60)
-    plt.suptitle('Zero-shot results')
+    ax.tick_params(axis='x', rotation=40)
+    title = filename.replace('_', ' ')
+    plt.suptitle(f'Zero-shot results', fontsize=14, fontweight='bold')
     plt.savefig(os.path.join(config.results_dir, FIGURES_DIR, f'all_metrics_{filename}.png'))
